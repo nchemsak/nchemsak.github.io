e6f14eeac7ba70fe6a1f3bca2cfc724dde292cf9
Nick <nchemsak@gmail.com>
2018-01-16 23:05:05 -0600

    heading bio update

--- js/creative.js
+++ js/creative.js
@@ -47,3 +47,13 @@
 
 
 })(jQuery); // End of use strict
+
+
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.background').css('top',-(scrolled*0.15)+'px');
+}
